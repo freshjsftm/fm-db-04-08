5a9982773a3ba466995e5c845a754e5bc1b4ea22
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-08-08 12:05:31 +0300

    Add client in node

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-// async function loadData() {
-//   try {
-//     const result = await fetch();
-//     const data = await result.json();
-//     return data;
-//   } 
-//   catch (error) {- первый вариант отлавливать ошибку
-//     console.log(error)
-//   }
-// }
+const { Client } = require("pg");
 
-//loadData().catch() - второй вариант отлавливать ошибку
+const config = {
+  user: "postgres",
+  password: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "fm_test",
+};
 
+const users = [
+  {
+    first_name: "Bred",
+    last_name: "Pit",
+    email: "pit2@gmail.com",
+    is_male: true,
+    birthday: "1963-06-28",
+    height: 1.92,
+  },
+  {
+    first_name: "Bred",
+    last_name: "Pit",
+    email: "pit3@gmail.com",
+    is_male: true,
+    birthday: "1963-06-28",
+    height: 1.92,
+  },
+  {
+    first_name: "Bred",
+    last_name: "Pit",
+    email: "pit4@gmail.com",
+    is_male: true,
+    birthday: "1963-06-28",
+    height: 1.92,
+  },
+];
+const client = new Client(config);
+start();
+
+async function start() {
+  await client.connect();
+
+  const result = await client.query(`
+  INSERT INTO "users"("first_name","last_name","email","is_male","birthday","height") 
+  VALUES ${mapUsers(users)};
+  `);
+  
+  await client.end();
+}
+
+function mapUsers(users) {
+  return users
+    .map(
+      (user) =>
+        `('${user.first_name}', '${user.last_name}', '${user.email}', '${user.is_male}', '${user.birthday}', '${user.height}')`
+    )
+    .join(",");
+}
