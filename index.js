c4ff979857b130a4e2bbbc20275ec55fa6153070
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-08-08 11:20:51 +0300

    Init npm, add pg & axios

---
+++ index.js
@@ -0,0 +1,13 @@
+// async function loadData() {
+//   try {
+//     const result = await fetch();
+//     const data = await result.json();
+//     return data;
+//   } 
+//   catch (error) {- первый вариант отлавливать ошибку
+//     console.log(error)
+//   }
+// }
+
+//loadData().catch() - второй вариант отлавливать ошибку
+
