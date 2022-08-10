3961617c307403c1675a212686748a9cf2ab909e
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-08-10 12:45:11 +0300

    Add agregate tasks

--- agregate.sql
+++ agregate.sql
@@ -28,3 +28,67 @@ SELECT count(*) , sum("weight") AS "sum weight", "first_name"
 FROM "users"
 WHERE "first_name" IN ('Alex', 'David', 'Max')
 GROUP BY "first_name";
+
+SELECT count(*), sum("weight"), "is_male"
+FROM "users"
+GROUP BY("is_male");
+
+
+
+SELECT max("height")
+FROM"users"
+WHERE "is_male" =true AND "weight">200;
+
+
+
+
+
+--3
+SELECT  min("height") 
+FROM "users" 
+WHERE EXTRACT("months" FROM "birthday") = 1;
+
+--4
+SELECT avg ("height"), "is_male"
+FROM "users"
+WHERE ...
+GROUP BY "is_male";
+
+--6. 
+SELECT count(*), EXTRACT ("years" from "birthday") AS "years"
+FROM "users"
+WHERE EXTRACT ("years" from "birthday") IN (1992,2001)
+GROUP BY "years";
+
+SELECT *
+FROM (
+    SELECT count(*), 
+    EXTRACT ("years" from "birthday") AS "years"
+    FROM "users" 
+    GROUP BY "years"
+  ) AS "tuy"
+WHERE "tuy"."years" IN (1992,2001);
+
+
+
+
+--7
+SELECT sum("height"), 
+  EXTRACT("months" FROM "birthday") AS "month" 
+FROM "users" 
+GROUP BY "month";
+
+
+
+
+
+
+
+
+SELECT "height",count(*)
+FROM "users"
+GROUP BY "height"
+LIMIT 10;
+
+
+
